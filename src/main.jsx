f8fc972e27b9f754aeb60dcc71ee5e200ed1e943
bgithinji927-source <2.62744309e+08+bgithinji927-source@users.noreply.github.com>
2026-08-25 07:41:31 +0000

    Add non-Kai interface animations

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./dark-theme.css";
+import "./nonKaiAnimations.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
