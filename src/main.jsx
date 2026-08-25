10c814a1ae7a2d2ed73d9b4423f9f169f037d80a
bgithinji927-source <2.62744309e+08+bgithinji927-source@users.noreply.github.com>
2026-08-25 08:32:45 +0000

    Add White and Purple typography system

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./dark-theme.css";
 import "./nonKaiAnimations.css";
+import "./whitePurpleTypography.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
